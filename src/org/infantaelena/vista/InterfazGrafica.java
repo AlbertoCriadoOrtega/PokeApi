73242489625e38fb75000d6c85c75f1485bdfcee
sergioperez1234 <sergio.perez200210@gmail.com>
2023-05-08 11:54:40 +0200

    Dia 1 Sergio

---
+++ src/org/infantaelena/vista/InterfazGrafica.java
@@ -0,0 +1,10 @@
+package org.infantaelena.vista;
+
+import javax.swing.*;
+
+public class InterfazGrafica extends JFrame {
+
+    private final int WIDTH = 600;
+    private final int HEIGHT = 600;
+
+}
