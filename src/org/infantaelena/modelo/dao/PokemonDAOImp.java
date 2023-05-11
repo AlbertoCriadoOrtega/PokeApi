1477646ccac849770352632947e026e2d36cd859
alumnoDAW <albertocriadoortega@gmail.com>
2023-05-11 11:58:40 +0200

    Dia 2 Alberto Criado

--- src/org/infantaelena/modelo/dao/PokemonDAOImp.java
+++ src/org/infantaelena/modelo/dao/PokemonDAOImp.java
@@ -1,5 +1,6 @@
 package org.infantaelena.modelo.dao;
 
+
 import org.infantaelena.excepciones.PokemonNotFoundException;
 import org.infantaelena.excepciones.PokemonRepeatedException;
 import org.infantaelena.modelo.entidades.Pokemon;
@@ -18,17 +19,24 @@ import java.util.List;
  */
 public class PokemonDAOImp implements PokemonDAO{
 
+    private Connection conection;
 
+    public PokemonDAOImp() {
+        try {
+            this.conection = DriverManager.getConnection("jdbc:sqlite:bbdd/personas.db");
+            System.out.println("Conexión establecida con la base de datos");
+        } catch (SQLException e) {
+            System.err.println("Error al conectar con la base de datos");
+        }
+    }
 
     @Override
     public void crear(Pokemon pokemon) throws PokemonRepeatedException {
-        try {
-            Connection conexion = DriverManager.getConnection("jdbc:mysql://192.168.56.101/empleados?user=admin00&password=alumno");
+        try(Statement st = conection.createStatement()){
+            st.executeUpdate("INSERT INTO personas (nombre) VALUES ('"+Pokemon.+"')");
         } catch (SQLException e){
-
+            System.err.println("Error al insertar persona");
         }
-
-
     }
 
     @Override
