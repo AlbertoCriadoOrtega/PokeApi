e747251088633df7e63fad6ca7160812afcd80f1
sergioperez1234 <sergio.perez200210@gmail.com>
2023-05-28 20:57:02 +0200

    Dia 5 Sergio

--- src/main/java/org/infantaelena/vista/InterfazGrafica.java
+++ src/main/java/org/infantaelena/vista/InterfazGrafica.java
@@ -1,16 +1,21 @@
 package org.infantaelena.vista;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class InterfazGrafica extends JFrame {
 
-    private final int ANCHO = 600;
+    private final int ANCHO = 800;
     private final int ALTO = 600;
 
     private JTextField nombre;
-    private JTextField apellido;
-    private JTextField numReg;
+    private JTextField tipo;
+    private JTextField hp;
+    private JTextField ataque;
+    private JTextField defensa;
+    private JTextField velocidad;
+    private DefaultTableModel atributosTableModel;
 
     public InterfazGrafica(){
         setTitle("PokeAPI");
@@ -29,35 +34,83 @@ public class InterfazGrafica extends JFrame {
 
         JPanel panelTipo = new JPanel();
         panelTipo.add(new JLabel("Tipo:"));
-        apellido = new JTextField(20);
-        panelTipo.add(apellido);
+        tipo = new JTextField(20);
+        panelTipo.add(tipo);
         panelDatos.add(panelTipo);
 
         JPanel panelHP = new JPanel();
         panelHP.add(new JLabel("HP:"));
-        numReg = new JTextField(20);
-        panelHP.add(numReg);
+        hp = new JTextField(20);
+        panelHP.add(hp);
         panelDatos.add(panelHP);
 
         JPanel panelAtaque = new JPanel();
         panelAtaque.add(new JLabel("Ataque:"));
-        numReg = new JTextField(20);
-        panelAtaque.add(numReg);
+        ataque = new JTextField(20);
+        panelAtaque.add(ataque);
         panelDatos.add(panelAtaque);
 
         JPanel panelDefensa = new JPanel();
         panelDefensa.add(new JLabel("Defensa:"));
-        numReg = new JTextField(20);
-        panelDefensa.add(numReg);
+        defensa = new JTextField(20);
+        panelDefensa.add(defensa);
         panelDatos.add(panelDefensa);
 
         JPanel panelVelocidad = new JPanel();
         panelVelocidad.add(new JLabel("Velocidad:"));
-        numReg = new JTextField(20);
-        panelVelocidad.add(numReg);
+        velocidad = new JTextField(20);
+        panelVelocidad.add(velocidad);
         panelDatos.add(panelVelocidad);
 
         add(panelDatos, BorderLayout.WEST);
+
+        JPanel panelTabla = new JPanel();
+
+        atributosTableModel = new DefaultTableModel();
+        atributosTableModel.addColumn("Nombre");
+        atributosTableModel.addColumn("Tipo");
+        atributosTableModel.addColumn("HP");
+        atributosTableModel.addColumn("Ataque");
+        atributosTableModel.addColumn("Defensa");
+        atributosTableModel.addColumn("Velocidad");
+
+        JTable atributosTable = new JTable(atributosTableModel);
+        JScrollPane atributosScrollPane = new JScrollPane(atributosTable);
+        panelTabla.add(atributosScrollPane);
+
+        add(panelTabla, BorderLayout.EAST);
+
+        JPanel panelBoton = new JPanel();
+
+        JButton btnCrear = new JButton("Crear");
+        panelBoton.add(btnCrear);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnBorrar = new JButton("Borrar");
+        panelBoton.add(btnBorrar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnListar = new JButton("Listar");
+        panelBoton.add(btnListar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnFiltrar = new JButton("Filtrar");
+        panelBoton.add(btnFiltrar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnModificar = new JButton("Modificar");
+        panelBoton.add(btnModificar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnGuardarBBDD = new JButton("Guardar BBDD");
+        panelBoton.add(btnGuardarBBDD);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnCargarBBDD = new JButton("Cargar BBDD");
+        panelBoton.add(btnCargarBBDD);
+        add(panelBoton, BorderLayout.SOUTH);
+
+
 
         JPanel panelVacio = new JPanel();
         add(panelVacio, BorderLayout.CENTER);
--- src/main/java/org/infantaelena/vista/Vista.java
+++ src/main/java/org/infantaelena/vista/Vista.java
@@ -1,5 +1,9 @@
 package org.infantaelena.vista;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
 /**
  * Clase que representa la vista de la aplicación
  * Implementar con menus de texto o con interfaz gráfica
@@ -8,5 +12,115 @@ package org.infantaelena.vista;
  * @date 24/04/2023
  *
  */
-public class Vista {
+public class Vista extends JFrame{
+    private final int ANCHO = 800;
+    private final int ALTO = 600;
+
+    private JTextField nombre;
+    private JTextField tipo;
+    private JTextField hp;
+    private JTextField ataque;
+    private JTextField defensa;
+    private JTextField velocidad;
+    private DefaultTableModel atributosTableModel;
+
+    public Vista(){
+        setTitle("PokeAPI");
+        setSize(ANCHO, ALTO);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel panelDatos = new JPanel();
+        panelDatos.setLayout(new BoxLayout(panelDatos, BoxLayout.Y_AXIS));
+
+        JPanel panelNombre = new JPanel();
+        panelNombre.add(new JLabel("Nombre:"));
+        nombre = new JTextField(20);
+        panelNombre.add(nombre);
+        panelDatos.add(panelNombre);
+
+        JPanel panelTipo = new JPanel();
+        panelTipo.add(new JLabel("Tipo:"));
+        tipo = new JTextField(20);
+        panelTipo.add(tipo);
+        panelDatos.add(panelTipo);
+
+        JPanel panelHP = new JPanel();
+        panelHP.add(new JLabel("HP:"));
+        hp = new JTextField(20);
+        panelHP.add(hp);
+        panelDatos.add(panelHP);
+
+        JPanel panelAtaque = new JPanel();
+        panelAtaque.add(new JLabel("Ataque:"));
+        ataque = new JTextField(20);
+        panelAtaque.add(ataque);
+        panelDatos.add(panelAtaque);
+
+        JPanel panelDefensa = new JPanel();
+        panelDefensa.add(new JLabel("Defensa:"));
+        defensa = new JTextField(20);
+        panelDefensa.add(defensa);
+        panelDatos.add(panelDefensa);
+
+        JPanel panelVelocidad = new JPanel();
+        panelVelocidad.add(new JLabel("Velocidad:"));
+        velocidad = new JTextField(20);
+        panelVelocidad.add(velocidad);
+        panelDatos.add(panelVelocidad);
+
+        add(panelDatos, BorderLayout.WEST);
+
+        JPanel panelTabla = new JPanel();
+
+        atributosTableModel = new DefaultTableModel();
+        atributosTableModel.addColumn("Nombre");
+        atributosTableModel.addColumn("Tipo");
+        atributosTableModel.addColumn("HP");
+        atributosTableModel.addColumn("Ataque");
+        atributosTableModel.addColumn("Defensa");
+        atributosTableModel.addColumn("Velocidad");
+
+        JTable atributosTable = new JTable(atributosTableModel);
+        JScrollPane atributosScrollPane = new JScrollPane(atributosTable);
+        panelTabla.add(atributosScrollPane);
+
+        add(panelTabla, BorderLayout.EAST);
+
+        JPanel panelBoton = new JPanel();
+
+        JButton btnCrear = new JButton("Crear");
+        panelBoton.add(btnCrear);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnBorrar = new JButton("Borrar");
+        panelBoton.add(btnBorrar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnListar = new JButton("Listar");
+        panelBoton.add(btnListar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnFiltrar = new JButton("Filtrar");
+        panelBoton.add(btnFiltrar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnModificar = new JButton("Modificar");
+        panelBoton.add(btnModificar);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnGuardarBBDD = new JButton("Guardar BBDD");
+        panelBoton.add(btnGuardarBBDD);
+        add(panelBoton, BorderLayout.SOUTH);
+
+        JButton btnCargarBBDD = new JButton("Cargar BBDD");
+        panelBoton.add(btnCargarBBDD);
+        add(panelBoton, BorderLayout.SOUTH);
+
+
+
+        JPanel panelVacio = new JPanel();
+        add(panelVacio, BorderLayout.CENTER);
+        setVisible(true);
+    }
 }
